38d11fd5ebcec5265741d66546a59956293af1b2
Vladislav Suratov <suratovvlad@gmail.com>
2019-01-03 21:45:28 +0300

    Add to about dialog info about current version

--- src/AboutDarkThemePluginDialog.cpp
+++ src/AboutDarkThemePluginDialog.cpp
@@ -1,6 +1,8 @@
 #include "AboutDarkThemePluginDialog.h"
 #include "ui_AboutDarkThemePluginDialog.h"
 
+#include <string>
+
 using namespace libqdark;
 
 AboutDarkThemePluginDialog::AboutDarkThemePluginDialog(QWidget* parent)
@@ -9,12 +11,27 @@ AboutDarkThemePluginDialog::AboutDarkThemePluginDialog(QWidget* parent)
 {
     ui->setupUi(this);
 
-    //ui->nameLabel->setText(m_ui->nameLabel->text() + " " + KEEPASSX_VERSION);
+    ui->nameLabel->setText(ui->nameLabel->text() + " " + LIBQDARK_VERSION);
 
     auto nameLabelFont = ui->nameLabel->font();
     nameLabelFont.setBold(true);
     nameLabelFont.setPointSize(nameLabelFont.pointSize() + 4);
     ui->nameLabel->setFont(nameLabelFont);
+
+    QString commitHash;
+    if (!QString(GIT_VERSION).isEmpty()) {
+        commitHash = GIT_VERSION;
+    }
+
+    if (!commitHash.isEmpty()) {
+        QString labelText = tr("Revision").append(": ").append(commitHash);
+        ui->label_git->setText(labelText);
+    }
+
+    auto libs = QString("%1\n- Qt %2")
+            .arg(ui->label_libs->text())
+            .arg(QString::fromLocal8Bit(qVersion()));
+    ui->label_libs->setText(libs);
 }
 
 AboutDarkThemePluginDialog::~AboutDarkThemePluginDialog() = default;
